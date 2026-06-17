c8e20df640cd5fd0795b56566e8d869cb987b2ae
Janvi Bhanushali <bhanushalijanvi357@gmail.com>
2026-06-17 13:46:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-       origin: 'https://outfit-suggestor-kzmj.vercel.app/',
+       origin: 'https://outfit-suggestor-kzmj.vercel.app',
        credentials: true
    }));
 app.use(express.json());
